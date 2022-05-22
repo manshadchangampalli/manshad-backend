e4641aaf8259e73c1780942f8cd7a820796135eb
manshad <manshadchangampalli010@gmail.com>
2022-05-22 09:50:11 +0530

    #added: all changes added

--- Connection/connect.js
+++ Connection/connect.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require('dotenv').config()
 
 const con = mongoose
   .connect(
-    "mongodb+srv://manshad:kq2wBbptBmvAJDWs@cluster0.pj5m8.mongodb.net/manshadDB?retryWrites=true&w=majority",
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pj5m8.mongodb.net/manshadDB?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
